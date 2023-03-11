53bc70bf2391878ef406eb3cfe1b9aae23212ce9
Alchimiștii Elixir <alchimistii.elixir@gmail.com>
2023-03-11 18:07:30 +0200

    AccountV1Esdt example

--- config/test.exs
+++ config/test.exs
@@ -6,4 +6,5 @@ config :logger, level: :warn, pretty: true
 # Faucet config
 config :elrondex, Elrondex.Faucet,
   active: true,
-  faucet_mnemonic: System.get_env("FAUCET_MNEMONIC")
+  faucet_mnemonic: System.get_env("FAUCET_MNEMONIC"),
+  esdt_token: "XPROVER-37f217"
---
+++ test/devnet/account_v1_esdt_test.exs
@@ -0,0 +1,62 @@
+defmodule Devnet.AccountV1EsdtTest do
+  alias Elrondex.{ESDT, Network, Faucet, Transaction, REST, Account}
+  alias Xprover.AccountState
+  alias Xprover.Test.{Alice, Bob}
+
+  use ExUnit.Case
+
+  @tag timeout: :infinity
+  test "transfer 1 EGDL from Alice to AccountV1" do
+    devnet = Network.get(:devnet)
+
+    faucet = Faucet.get_account!()
+
+    identifier = Keyword.get(Application.get_env(:elrondex, Elrondex.Faucet), :esdt_token)
+    esdt = %ESDT{identifier: identifier}
+
+    # TODO Add tr for gas !
+    {:ok, tr0} =
+      ESDT.transfer(faucet, Alice.address(), esdt, 1_000_000)
+      |> Transaction.prepare(devnet)
+      |> Transaction.sign()
+      |> REST.post_transaction_send()
+      |> Transaction.wait(devnet, withResults: true)
+
+    IO.inspect(tr0)
+
+    # prepare, sign and send transaction
+    account_v1 = Account.generate_random()
+
+    {:ok, tr1} =
+      ESDT.transfer(Alice.account(), account_v1.address, esdt, 1_000_000)
+      |> Transaction.prepare(devnet)
+      |> Transaction.sign()
+      |> REST.post_transaction_send()
+      |> Transaction.wait(devnet, withResults: true)
+
+    IO.inspect(tr1)
+
+    {:ok, account_v1_state} =
+      AccountState.get_current(
+        account_v1.address,
+        devnet,
+        version: 1,
+        tx_hash: tr1["hash"]
+      )
+
+    account_v1_history = %{
+      network: :devnet,
+      address: account_v1.address,
+      versions: [account_v1_state]
+    }
+
+    IO.inspect(account_v1_history)
+
+    TestData.put_data(
+      :devnet,
+      :account_history,
+      "#{account_v1.address}.json",
+      account_v1_history
+    )
+  end
+end
