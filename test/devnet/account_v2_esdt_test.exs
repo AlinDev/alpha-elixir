a7333f5aeac50dc24ded6b042f71f2e807dded43
Alchimiștii Elixir <alchimistii.elixir@gmail.com>
2023-03-11 18:22:54 +0200

    AccountV2Esdt example

--- test/devnet/account_v2_esdt_test.exs
+++ test/devnet/account_v2_esdt_test.exs
@@ -1,4 +1,4 @@
-defmodule Devnet.AccountV1EsdtTest do
+defmodule Devnet.AccountV2EsdtTest do
   alias Elrondex.{ESDT, Network, Faucet, Transaction, REST, Account}
   alias Xprover.AccountState
   alias Xprover.Test.{Alice, Bob}
@@ -6,7 +6,7 @@ defmodule Devnet.AccountV1EsdtTest do
   use ExUnit.Case
 
   @tag timeout: :infinity
-  test "transfer 1 XPROVER token from Alice to AccountV1" do
+  test "transfer 2 XPROVER tokens from Alice to AccountV2 in 2 transactions" do
     devnet = Network.get(:devnet)
 
     faucet = Faucet.get_account!()
@@ -14,9 +14,9 @@ defmodule Devnet.AccountV1EsdtTest do
     identifier = Keyword.get(Application.get_env(:elrondex, Elrondex.Faucet), :esdt_token)
     esdt = %ESDT{identifier: identifier}
 
-    # TODO Add tr for gas !
+    #
     {:ok, tr0} =
-      ESDT.transfer(faucet, Alice.address(), esdt, 1_000_000)
+      ESDT.transfer(faucet, Alice.address(), esdt, 2_000_000)
       |> Transaction.prepare(devnet)
       |> Transaction.sign()
       |> REST.post_transaction_send()
@@ -25,10 +25,10 @@ defmodule Devnet.AccountV1EsdtTest do
     IO.inspect(tr0)
 
     # prepare, sign and send transaction
-    account_v1 = Account.generate_random()
+    account_v2 = Account.generate_random()
 
     {:ok, tr1} =
-      ESDT.transfer(Alice.account(), account_v1.address, esdt, 1_000_000)
+      ESDT.transfer(Alice.account(), account_v2.address, esdt, 1_000_000)
       |> Transaction.prepare(devnet)
       |> Transaction.sign()
       |> REST.post_transaction_send()
@@ -36,27 +36,44 @@ defmodule Devnet.AccountV1EsdtTest do
 
     IO.inspect(tr1)
 
-    {:ok, account_v1_state} =
+    {:ok, account_v2_state1} =
       AccountState.get_current(
-        account_v1.address,
+        account_v2.address,
         devnet,
         version: 1,
         tx_hash: tr1["hash"]
       )
 
-    account_v1_history = %{
+    {:ok, tr2} =
+      ESDT.transfer(Alice.account(), account_v2.address, esdt, 1_000_000)
+      |> Transaction.prepare(devnet)
+      |> Transaction.sign()
+      |> REST.post_transaction_send()
+      |> Transaction.wait(devnet, withResults: true)
+
+    IO.inspect(tr2)
+
+    {:ok, account_v2_state2} =
+      AccountState.get_current(
+        account_v2.address,
+        devnet,
+        version: 2,
+        tx_hash: tr2["hash"]
+      )
+
+    account_v2_history = %{
       network: :devnet,
-      address: account_v1.address,
-      versions: [account_v1_state]
+      address: account_v2.address,
+      versions: [account_v2_state2, account_v2_state1]
     }
 
-    IO.inspect(account_v1_history)
+    IO.inspect(account_v2_history)
 
     TestData.put_data(
       :devnet,
       :account_history,
-      "#{account_v1.address}.json",
-      account_v1_history
+      "#{account_v2.address}.json",
+      account_v2_history
     )
   end
 end
