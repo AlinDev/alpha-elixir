500c57b1d0ba5b8ca5bf9bb717189bd375f9d799
Alchimiștii Elixir <alchimistii.elixir@gmail.com>
2023-03-11 15:39:53 +0200

    Xprover.AccountState.get_tokens/2 impl

--- lib/xprover/account_state.ex
+++ lib/xprover/account_state.ex
@@ -1,10 +1,60 @@
 defmodule Xprover.AccountState do
-  alias Xprover.AccountState
+  alias Xprover.{AccountState, Token}
+  alias Elrondex.{Network, REST}
 
   defstruct nonce: 0,
             version: 0,
             balance: 0,
             tx_hash: nil
+
+  # EGLD
+
+  @doc """
+  Return current account tokens list.
+  List contain EGLD as first token.
+  """
+  def get_tokens(address, %Network{} = network) do
+    with {:ok, acc} <- REST.get_address(network, address),
+         {:ok, esdt_map} <- REST.get_address_esdt(network, address),
+         {:ok, esdt_tokens} <- parse_tokens(esdt_map, []) do
+      native = %Token{identifier: "EGLD", name: "Native EGLD token", balance: acc["balance"]}
+      [native | esdt_tokens]
+    else
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp parse_tokens([], acc_tokens) when is_list(acc_tokens) do
+    {:ok, acc_tokens}
+  end
+
+  defp parse_tokens(esdt_map, acc_tokens) when is_map(esdt_map) do
+    IO.inspect(esdt_map)
+
+    esdt_only =
+      esdt_map
+      |> Enum.filter(fn {k, v} -> length(String.split(k, "-")) == 2 end)
+      |> Enum.map(fn {k, v} -> v end)
+
+    IO.inspect(esdt_only)
+
+    parse_tokens(esdt_only, acc_tokens)
+  end
+
+  defp parse_tokens([h_token | t_tokens], acc_tokens) do
+    case parse(h_token) do
+      {:ok, token} -> parse_tokens(t_tokens, [token | acc_tokens])
+      {:error, error} -> {:error, error}
+    end
+  end
+
+  defp parse(%{"balance" => balance, "tokenIdentifier" => identifier}) do
+    {:ok, %Token{identifier: identifier, balance: balance}}
+  end
+
+  defp parse(_acc_token) do
+    {:error, :parse_error}
+  end
 
   def to_json(%AccountState{balance: balance} = account_state) when is_integer(balance) do
     %{account_state | balance: "#{balance}"}
--- test/devnet/esdt_test.exs
+++ test/devnet/esdt_test.exs
@@ -1,7 +1,7 @@
 defmodule Devnet.EsdtTest do
   alias Elrondex.{Network, Faucet, Transaction, REST, Account}
   alias Xprover.Test.{Alice, Bob}
-  alias Xprover.Token
+  alias Xprover.{AccountState, Token}
 
   use ExUnit.Case
 
@@ -20,6 +20,7 @@ defmodule Devnet.EsdtTest do
     IO.inspect(esdt)
   end
 
+  @tag :skip
   test "test token encode" do
     token = %Token{identifier: "RIDE-6e4c49", balance: 100_000}
     json = Jason.encode!(token, pretty: true)
@@ -31,5 +32,59 @@ defmodule Devnet.EsdtTest do
     #   "name": "Name for RIDE-6e4c49"
     # }
     #
+  end
+
+  @tag :skip
+  test "get account balance" do
+    devnet = Network.get(:devnet)
+    address = "erd1c579aq6kjkhar6cyx8qm9k9wettxjcf3jncduwe0pllqx0rlczfskg3vk2"
+    {:ok, acc} = REST.get_address(devnet, address)
+    IO.inspect(acc)
+
+    # %{
+    #  "address" => "erd1c579aq6kjkhar6cyx8qm9k9wettxjcf3jncduwe0pllqx0rlczfskg3vk2",
+    #  "balance" => "9280416133978549558458",
+    #  "code" => "",
+    #  "codeHash" => nil,
+    #  "codeMetadata" => nil,
+    #  "developerReward" => "0",
+    #  "nonce" => 181,
+    #  "ownerAddress" => "",
+    #  "rootHash" => "rt0E3avL6eSmhCwb0W/bjBeLUXDoHMiZlIg5pYvcBGo=",
+    #  "username" => ""
+    # }
+  end
+
+  test "get tokens" do
+    devnet = Network.get(:devnet)
+    address = "erd1c579aq6kjkhar6cyx8qm9k9wettxjcf3jncduwe0pllqx0rlczfskg3vk2"
+    tokens = AccountState.get_tokens(address, devnet)
+    # IO.inspect(tokens)
+
+    json = Jason.encode!(tokens, pretty: true)
+    IO.puts(json)
+
+    # [
+    #  {
+    #    "balance": "9280416133978549558458",
+    #    "identifier": "EGLD",
+    #    "name": "Native EGLD token"
+    #  },
+    #  {
+    #    "balance": "200000000000000000",
+    #    "identifier": "XZPAY-ab2d80",
+    #    "name": "Name for XZPAY-ab2d80"
+    #  },
+    #  {
+    #    "balance": "6826076761875767981",
+    #    "identifier": "WEGLD-d7c6bb",
+    #    "name": "Name for WEGLD-d7c6bb"
+    #  },
+    #  {
+    #    "balance": "2927152819462113",
+    #    "identifier": "WEB-5d08be",
+    #    "name": "Name for WEB-5d08be"
+    #  }
+    # ]
   end
 end
